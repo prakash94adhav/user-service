7940f6ae253263b23ed26ea22d794c5785ff53f0
prakash94adhav <adhavprakash404@gmail.com>
2022-11-27 02:06:41 +0530

    added application.yml, pom.xml, Rating.java, UserServiceApplication.java for eureka server

--- src/main/java/com/user/entity/Rating.java
+++ src/main/java/com/user/entity/Rating.java
@@ -4,11 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Id;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Rating {
 
+    @Id
     private String ratingId;
     private String userId;
     private String hotelId;
